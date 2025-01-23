b74e90a27c95a0fd9c20eb8907d89d9e8775d696
mccoy88f <50622699+mccoy88f@users.noreply.github.com>
2025-01-23 19:36:06 +0100

    Create index.js

---
+++ frontend/src/components/playlist/index.js
@@ -0,0 +1,6 @@
+export { PlaylistList } from './PlaylistList';
+export { PlaylistForm } from './PlaylistForm';
+export { PlaylistDetails } from './PlaylistDetails';
+export { ChannelList } from './ChannelList';
+export { ChannelForm } from './ChannelForm';
+export { ChannelCard } from './ChannelCard';
